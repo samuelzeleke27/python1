23a53e3e2e6212eab2f8dbd39bc377f1100c1658
Samuel Zeleke <samuelzeleke24@yahoo.com>
2023-10-28 20:53:52 -0400

    initial code

---
+++ Exer1.py
@@ -0,0 +1,14 @@
+#dor, bir, kir = 3, 4, 6
+#g= bir + dor
+#k = bir + kir
+#x = y= z = 40
+#k = x + y + z
+#fName = "Sami"
+#lName = "Zelek"
+#print("The result is ", lName + " " + fName)
+a = 12
+b = 9
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
